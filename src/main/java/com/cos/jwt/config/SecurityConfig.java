09c2270abb27d6bb9f1ab7969fb9c386e2b97ef6
JadenKim-dev <jaden.kim5179@gmail.com>
2022-03-11 16:51:39 +0900

    jwt 토큰 검증 로직 : JwtAuthorizationFilter - doFilterInternal()

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cos.jwt.config;
 import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
 import com.cos.jwt.config.jwt.JwtAuthorizationFilter;
 import com.cos.jwt.filter.MyFilter3;
+import com.cos.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsConfig corsConfig;
+    private final UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(corsConfig.corsFilter())  // 모든 요청이 corsFilter를 거치게 됨, 서버는 cors 정책에서 벗어날 수 있음
                 // @CrossOrigin -> 인증이 없을 때 / 시큐리티 필터에 등록 -> 인증이 있을 때
-                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userRepository))
                 .formLogin().disable() // 폼 로그인 방식 쓰지 않음
                 .httpBasic().disable() // 기본적인 http 방식 쓰지 않음
                 .addFilter(new JwtAuthenticationFilter(authenticationManager())) // AuthenticationManager가 필요
