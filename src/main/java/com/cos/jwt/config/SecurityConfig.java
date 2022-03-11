8aa96c67c6fa47f0e63f0c7dc1b15e6bcac0a304
JadenKim-dev <jaden.kim5179@gmail.com>
2022-03-11 16:15:07 +0900

    change dependency : CorsFilter -> corsConfig.corsFilter()

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cos.jwt.config;
 
 import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
+import com.cos.jwt.config.jwt.JwtAuthorizationFilter;
 import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -8,24 +9,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CorsFilter corsFilter;
+    private final CorsConfig corsConfig;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+//        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않겠다는 설정
                 .and()
-                .addFilter(corsFilter)  // 모든 요청이 corsFilter를 거치게 됨, 서버는 cors 정책에서 벗어날 수 있음
+                .addFilter(corsConfig.corsFilter())  // 모든 요청이 corsFilter를 거치게 됨, 서버는 cors 정책에서 벗어날 수 있음
                 // @CrossOrigin -> 인증이 없을 때 / 시큐리티 필터에 등록 -> 인증이 있을 때
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
                 .formLogin().disable() // 폼 로그인 방식 쓰지 않음
                 .httpBasic().disable() // 기본적인 http 방식 쓰지 않음
                 .addFilter(new JwtAuthenticationFilter(authenticationManager())) // AuthenticationManager가 필요
